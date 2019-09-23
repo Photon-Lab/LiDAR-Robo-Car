b1621a475ceff18b9243d4e6c4d9a6b6e33f92ae
Roland Podlucki <s1558723@sms.ed.ac.uk>
2019-09-23 13:50:07 +0100

    Replace multiple_sensors.ino

--- src/sensor communication C/multiple_sensors.ino
+++ src/sensor communication C/multiple_sensors.ino
@@ -3,9 +3,13 @@
 
 VL53L0X sensor1;
 VL53L0X sensor2;
+VL53L0X sensor3;
+VL53L0X sensor4;
 
 int r1;
 int r2;
+int r3;
+int r4;
 
 void setup() {
   Serial.begin(9600);
@@ -13,13 +17,21 @@ void setup() {
 
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
 
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
 
   delay(500);
 
@@ -35,10 +47,32 @@ void setup() {
   Serial.println("sensor2");
   sensor2.init();
 
+  delay(100);
+
   sensor2.setAddress((uint8_t)02);
+
+  digitalWrite(4, HIGH);
+  Serial.println("sensor3");
+  sensor3.init();
+
+  delay(100);
+
+  sensor3.setAddress((uint8_t)03);
+
+  digitalWrite(5, HIGH);
+  Serial.println("sensor4");
+  sensor4.init();
+
+  delay(100);
+
+  sensor4.setAddress((uint8_t)04);
+
+  delay(100);
 
   sensor1.startContinuous();
   sensor2.startContinuous();
+  sensor3.startContinuous();
+  sensor4.startContinuous();
 }
 
 void loop() {
@@ -55,6 +89,20 @@ void loop() {
 
   if(r2>50){digitalWrite(7, HIGH);}
     else{digitalWrite(7, LOW);}
+
+  r3=sensor3.readRangeContinuousMillimeters();
+  Serial.print(r3);
+  Serial.print(" ");
+
+  if(r3>50){digitalWrite(8, HIGH);}
+    else{digitalWrite(8, LOW);}
+
+  r4=sensor4.readRangeContinuousMillimeters();
+  Serial.print(r4);
+  Serial.print(" ");
+
+  if(r4>50){digitalWrite(9, HIGH);}
+    else{digitalWrite(9, LOW);}
 
   delay(100);
 }
